5b4a5176009356d1327454ea8adc5a83229167ef
Shaul Hameed <shaul21h@gmail.com>
2014-06-06 03:56:31 +0530

    Added updated Java file

--- Air_bone/gen/com/gelabs/air_bone/R.java
+++ Air_bone/gen/com/gelabs/air_bone/R.java
@@ -26,10 +26,12 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int ic_mic=0x7f020001;
+        public static final int lc_micglow=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
-        public static final int imageButton1=0x7f090000;
+        public static final int action_settings=0x7f090002;
+        public static final int speakbutton=0x7f090000;
+        public static final int speaknow=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -41,6 +43,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int todo=0x7f050003;
     }
     public static final class style {
         /** 
--- Air_bone/src/com/gelabs/air_bone/MainActivity.java
+++ Air_bone/src/com/gelabs/air_bone/MainActivity.java
@@ -3,6 +3,10 @@ package com.gelabs.air_bone;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ImageButton speakbutton = (ImageButton) findViewById(R.id.speakbutton);
+        final TextView speaknow = (TextView) findViewById(R.id.speaknow);
+        
+        speakbutton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				speakbutton.setImageResource(R.drawable.lc_micglow);
+				speaknow.setText("Speak now");
+			}
+		});
     }
 
     @Override
